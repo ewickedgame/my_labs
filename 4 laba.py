68acab54ee67c0107d0fd808d6691f29fb6f6656
ewickedgame <98728003+ewickedgame@users.noreply.github.com>
2022-12-25 12:44:26 +0300

    Update 4 laba.py

--- 4 laba.py
+++ 4 laba.py
@@ -1,56 +1,40 @@
-//////////////Задача1//////////
-uslov1 = [12, 1, 4, 8, 554, 87, 12, 90]
-print('Условие 1 задачи: ',uslov1)
-res1 = []
+///////////////задача1/////////////
+any_list = [1,2,3,4,5,6,7,8,9,10]
+result = []
+def odd_index(list):
+    for i in range(len(list)):
+        if i % 2 == 0:
+            result.append(list[i])
+    print(result)
+odd_index(any_list)
 
-def fun1 (list):
-    k = len(list)
-    for i in range(k):
-        if i%2 == 0:
-            res1.append( list[i] )
-    print('Ответ ',res1)
+///////////////задача2/////////////
+any_list = [1,2,3,4,5,6,7,8,9,10]
+result = []
+def more(list):
+    for i in range(len(list)):
+        if list[i] > list[i - 1]:
+            result.append(list[i])
+    print(result)
+more(any_list)
 
-fun1(uslov1)
-print()
+///////////////задача3/////////////
+any_list = [1,2,3,4,5,6,7,8,9,10]
+def replacement(list):
+    i_of_max_el = list.index(max(list))
+    i_of_min_el = list.index(min(list))
+    list[i_of_max_el], list[i_of_min_el] = list[i_of_min_el], list[i_of_max_el]
+    print(list)
+replacement(any_list)
 
+///////////////задача4/////////////
+d = {1: ':с', 2: 'Адам Смит', 3: 'Вторник'}
+key = int(input('Введите ключ, чтобы получить его значение: '))
+def get_value(dictionary):
+    value = dictionary.get(key)
+    print(value)
+get_value(d)
 
-//////////////Задача1/////////
-uslov2 = [1,5,2,7,9,1,2,4,6]
-print('Условие 2 задачи: ',uslov2)
-res2= []
-
-def fun2 (list):
-    k = len(list)
-    for i in range(1,k):
-        if list[i]>list[i-1]:
-            res2.append( list[i])
-    print('Ответ ',res2)
-
-fun2(uslov2)
-print()
-
-//////////////Задача3///////////
-uslov3 = [67,2,1,98,3,2,0]
-print('Условие 3 задачи: ',uslov3)
-
-def fun3 (list):
-    maxel = list.index(max(list))
-    minel = list.index(min(list))
-    list[maxel],list[minel]=list[minel],list[maxel]
-    print('Ответ ', uslov3)
-
-fun3(uslov3)
-print()
-
-
-
-//////////////Задача4///////////
-uslov4={1:"библиотека1",2:"библиотека2",3:"библиотека3"}
-klch=int(input('Введите ключ(от 1 до 3): '))
-
-def  fun4(dictionary):
-    res4=dictionary.get(klch)
-    print("Ответ ", res4)
-
-fun4(uslov4)
-print()
+///////////////задача5/////////////
+from figures import triangle_area
+triangle_area()
